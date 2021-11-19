c4d4a5a739a00ee31414311a0110c87d3e379d1c
Lévi Gendron <levi.gendron@hotmail.com>
2021-11-19 12:12:49 +0100

    Player class - first commit

---
+++ Player.py
@@ -0,0 +1,26 @@
+class Player:
+
+    def __init__(self, name):
+        self.__name = name
+        self.__cards = []
+
+    def __str__(self):
+        try:
+            print("Le joueur {} a en sa possession les cartes {} et {}".format(self.__name, self.__cards[0],
+                                                                               self.__cards[1]))
+        except IndexError:
+            print("Le joueur{} n'a pas de cartes".format(self.__name))
+
+    def get_cards(self):
+        return self.__cards
+
+    def set_cards(self, card1, card2):
+        self.__cards.append(card1)
+        self.__cards.append(card2)
+
+    def return_cards(self):
+        cards_buffer = self.__cards
+        self.__cards.clear()
+        return cards_buffer
+
+
